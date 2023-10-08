18dd28a0ef7b9a0bb8e46f1c682b9a039d2f5f85
Josué de Jesús Torres Torres <141877411+LincJ21@users.noreply.github.com>
2023-10-08 13:55:31 -0500

    conexión de paginas

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Obtener el enlace de "Servicios" por su ID
+var serviciosLink = document.getElementById("servicios-link");
+var hogarLink = document.getElementById("hogar-link");
+// Agregar un evento de clic al enlace de "Servicios"
+serviciosLink.addEventListener("click", function(event) {
+    // Prevenir el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Redirigir a la página deseada (por ejemplo, "pagina2.html")
+    window.location.href = "p1.html";
+});
+
+// Agregar un evento de clic al enlace de "Hogar"
+
+hogarLink.addEventListener("click", function(event) {
+    // Prevenir el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Redirigir a la página deseada (por ejemplo, "pagina2.html")
+    window.location.href = "index.html";
+});
